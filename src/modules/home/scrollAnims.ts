a6ca6cc14c3157b79ab03f449e9fae356636f02f
David Kanekanian <dkanekanian1@gmail.com>
2023-10-29 15:24:51 +0000

    save

--- src/modules/home/scrollAnims.ts
+++ src/modules/home/scrollAnims.ts
@@ -1,16 +1,26 @@
 import animate from "../../utils/animUtils";
 
 // create the animations
+// animate(
+//   ".logoWrap",
+//   { position: ["fixed", "relative"], translate: ["", `0 ${80 * 0.5}lvh`] },
+//   { timeRange: "exit 15% 50%", fill: "both", easing: "steps(1)" }
+// );
 animate(
-  ".logoWrap",
-  { position: ["fixed", "relative"], translate: ["", `0 ${80 * 0.5}lvh`] },
-  { timeRange: "exit 15% 50%", fill: "both", easing: "steps(1)" }
+  ".tagline",
+  { visibility: ["hidden", "visible", "visible", "visible", "hidden"], opacity: [0, 1, 1, 1, 0] },
+  { timeRange: "exit 0% 60%", fill: "both", easing: "ease-in" }
 );
 animate(
   ".tagline",
-  { visibility: ["hidden", "visible"], opacity: [0, 1] },
-  { timeRange: "exit 0% 20%", fill: "both", easing: "ease-in" }
-);
+  { marginTop: ["0lvh", "23vmin"] },
+  { timeRange: "exit 65% 80%", fill: "both", easing: "linear" }
+)
+animate(
+  "#logo",
+  {width: ["100vmin", "100px"]},
+  { timeRange: "exit 40% 80%", fill: "both", easing: "linear" }
+)
 
 // animate(
 //   ".splash",
