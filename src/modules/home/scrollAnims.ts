cf0d10ad22202c15ad4dbf9d598b583c4d7b62ca
David Kanekanian <dkanekanian1@gmail.com>
2023-10-29 14:46:11 +0000

    svae

--- src/modules/home/scrollAnims.ts
+++ src/modules/home/scrollAnims.ts
@@ -2,21 +2,21 @@ import animate from "../../utils/animUtils";
 
 // create the animations
 animate(
-  ".splashVignette",
-  { opacity: [0, 1] },
-  { timeRange: "exit 15% 35%", fill: "both", easing: "ease-in" }
+  ".logoWrap",
+  { position: ["fixed", "relative"], translate: ["", `0 ${80*.35}lvh`] },
+  { timeRange: "exit 15% 35%", fill: "both", easing: "steps(1)" }
 );
-animate(
-  ".splash",
-  { position: ["fixed", "relative"], translate: ["", "0 24lvh"] },
-  { timeRange: "exit 0% 30%", fill: "both", easing: "steps(1)" }
-);
-animate(
-  ".splash",
-  { translate: ["0 24lvh", "0 -20lvh"] },
-  {
-    timeRange: "exit 30% 100%",
-    fill: "forwards",
-    easing: "cubic-bezier(0.3, 0.3, 0.1, 1)",
-  }
-);
+// animate(
+//   ".splash",
+//   { position: ["fixed", "relative"], translate: ["", "0 24lvh"] },
+//   { timeRange: "exit 0% 30%", fill: "both", easing: "steps(1)" }
+// );
+// animate(
+//   ".splash",
+//   { translate: ["0 24lvh", "0 -20lvh"] },
+//   {
+//     timeRange: "exit 30% 100%",
+//     fill: "forwards",
+//     easing: "cubic-bezier(0.3, 0.3, 0.1, 1)",
+//   }
+// );
