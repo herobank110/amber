1114002a0f27a6a0e37f04f18e9fd5edcff2da07
David Kanekanian <dkanekanian1@gmail.com>
2023-10-29 15:02:37 +0000

    taglien

--- src/modules/home/scrollAnims.ts
+++ src/modules/home/scrollAnims.ts
@@ -3,9 +3,15 @@ import animate from "../../utils/animUtils";
 // create the animations
 animate(
   ".logoWrap",
-  { position: ["fixed", "relative"], translate: ["", `0 ${80*.35}lvh`] },
-  { timeRange: "exit 15% 35%", fill: "both", easing: "steps(1)" }
+  { position: ["fixed", "relative"], translate: ["", `0 ${80 * 0.5}lvh`] },
+  { timeRange: "exit 15% 50%", fill: "both", easing: "steps(1)" }
 );
+animate(
+  ".tagline",
+  { visibility: ["hidden", "visible"], opacity: [0, 1] },
+  { timeRange: "exit 0% 20%", fill: "both", easing: "ease-in" }
+);
+
 // animate(
 //   ".splash",
 //   { position: ["fixed", "relative"], translate: ["", "0 24lvh"] },
