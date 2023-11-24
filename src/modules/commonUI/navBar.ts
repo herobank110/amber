5a29dcb7d94e5290e0686725745a47ab473e08d5
David Kanekanian <dkanekanian1@gmail.com>
2023-11-24 17:23:41 +0000

    hamburger

--- src/modules/commonUI/navBar.ts
+++ src/modules/commonUI/navBar.ts
@@ -2,24 +2,35 @@ import $ from "jquery";
 import "./navBar.scss";
 
 export const makeNavBar = () =>
-  $("<nav>", {class: "navBar"}).append(
-    $("<ul>").append(
-      navItem({ text: "Upcoming", href: "/#upcoming" }),
-      navItem({ text: "Ten Years", href: "/#tenYears" }),
-      navItem({ text: "About Us", href: "/#about" }),
-      $("<a>", { href: "/", class: "logo" }),
-      navItem({ text: "Archive", href: "/?archive" }),
-      navItem({ text: "Contact Us", href: "/#contact" }),
-      $("<span>")
-    )
-  );
+  $("<nav>").append(deskopNav(), mobileNav());
 
 type NavItemProps = {
   text: string;
   href: string;
 };
 
+const deskopNav = () =>
+  $("<ul>", { class: "desktop" }).append(
+    navItem({ text: "Upcoming", href: "/#upcoming" }),
+    navItem({ text: "Ten Years", href: "/#tenYears" }),
+    navItem({ text: "About Us", href: "/#about" }),
+    logoItem(),
+    navItem({ text: "Archive", href: "/?archive" }),
+    navItem({ text: "Contact Us", href: "/#contact" }),
+    $("<span>")
+  );
+
 const navItem = (props: NavItemProps) =>
   $("<li>", { class: "navItem" }).append(
     $("<a>", { href: props.href, text: props.text })
+  );
+
+const logoItem = () =>
+  //
+  $("<a>", { href: "/", class: "logo" });
+
+const mobileNav = () =>
+  $("<div>", { class: "mobile" }).append(
+    $("<div>", { class: "hamburger" }),
+    logoItem(),
   );
