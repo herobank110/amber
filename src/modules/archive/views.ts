d12860d016a31ec1b8d5ca60dea7227aede7a133
David Kanekanian <dkanekanian1@gmail.com>
2023-12-04 12:25:23 +0000

    enable closing modal

--- src/modules/archive/views.ts
+++ src/modules/archive/views.ts
@@ -36,29 +36,30 @@ const concertCard = (props: Concert) =>
     .on("click", () => onClickConcertCard(props));
 
 const concertDetails = (props: Concert) =>
-  $("<div>", { class: "concertDetails" }).append(
-    $("<div>", { class: "popupInner" }).append(
-      $("<div>", { class: "posterWrap" }).append(
-        $("<div>", {
-          class: "poster",
-          css: { backgroundImage: `url('${props.poster}')` },
-        })
-      ),
-      $("<h3>", { text: props.title }),
-      $("<span>", { text: longDate(props.when) }),
-
-      props.facebook
-        ? $("<a>", {
-            class: "fbLink",
-            href: "https://www.facebook.com/events/1106400593274570/",
-          }).append(
-            //
-            facebookLogo(),
-            $("<span>", { text: "View on Facebook " })
-          )
-        : $()
+  $("<div>", { class: "concertDetails" })
+    .append(
+      $("<div>", { class: "popupInner" }).append(
+        $("<div>", { class: "posterWrap" }).append(
+          $("<div>", {
+            class: "poster",
+            css: { backgroundImage: `url('${props.poster}')` },
+          })
+        ),
+        $("<h3>", { text: props.title }),
+        $("<span>", { text: longDate(props.when) }),
+        props.facebook
+          ? $("<a>", {
+              class: "fbLink",
+              href: "https://www.facebook.com/events/1106400593274570/",
+            }).append(
+              //
+              facebookLogo(),
+              $("<span>", { text: "View on Facebook " })
+            )
+          : $()
+      )
     )
-  );
+    .on("click", onClickConcertDetails);
 
 function onClickConcertCard(concert: Concert) {
   const el = concertDetails(concert);
@@ -67,6 +68,17 @@ function onClickConcertCard(concert: Concert) {
     .css("--m", m + "px")
     .addClass("modal");
   el.appendTo("body");
+}
+
+function onClickConcertDetails(e: JQuery.ClickEvent) {
+  if (e.target == e.currentTarget)
+    // event was not in the outer part of the popup
+    hideDetails();
+}
+
+function hideDetails() {
+  $(".concertDetails").remove();
+  $("body").removeClass("modal")
 }
 
 function shortDate(when: string) {
