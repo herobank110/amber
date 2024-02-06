5460e61d6a278ee9d82e702d02deb161e1c2a42b
David Kanekanian <dkanekanian1@gmail.com>
2024-02-06 21:23:11 +0000

    Fix bug in login functionality

---
+++ scripts/db.sql
@@ -0,0 +1,18 @@
+set names utf8mb4;
+
+drop database if exists `amberDb`;
+create database `amberDb`;
+use `amberDb`;
+
+drop user if exists amberAdmin@localhost;
+create user amberAdmin@localhost identified by 'AMBER_ADMIN_PW';
+grant all on `amberDb`.* to amberAdmin@localhost;
+
+create table `concerts` (
+  `id` int not null auto_increment primary key,
+  `title` varchar(200) not null,
+  `when` datetime not null,
+  `thumb` varchar(100) not null,
+  `poster` varchar(100) not null,
+  `facebook` varchar(100) not null,
+) engine=InnoDB;
