d05871195c96c055ce3bcd43b43e2a459dc99984
David Kanekanian <dkanekanian1@gmail.com>
2024-02-21 09:06:56 +0000

    nice update

---
+++ src/php/concertEdit.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . '/db.php';
+
+$p_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+$p_title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
+$p_when = filter_input(INPUT_POST, 'when', FILTER_SANITIZE_SPECIAL_CHARS);
+$p_poster = filter_input(INPUT_POST, 'poster', FILTER_VALIDATE_URL);
+$p_thumb = filter_input(INPUT_POST, 'thumb', FILTER_VALIDATE_URL);
+$p_facebook = filter_input(INPUT_POST, 'facebook', FILTER_VALIDATE_URL);
+
+if (!$p_id || !$p_title || !$p_when || !$p_poster || !$p_thumb || !$p_facebook) {
+    http_response_code(400);
+    echo "One or more invalid or missing parameter(s)";
+    return;
+}
+
+function insertConcert() {
+    $statement = getOrCreateDb()->prepare(<<<SQL
+        INSERT INTO concerts (`title`, `when`, `poster`, `thumb`, `facebook`)
+        VALUES (?, ?, ?, ?, ?)
+        SQL);
+    $statement->bind_param("sssss", $p_title, $p_when, $p_poster, $p_thumb, $p_facebook);
+    return $statement->execute();
+}
+
+function updateConcert() {
+    $statement = getOrCreateDb()->prepare(<<<SQL
+        UPDATE concerts
+        SET `title` = ?, `when` = ?, `poster` = ?, `thumb` = ?, `facebook` = ?
+        WHERE `id` = ?
+        SQL);
+    $statement->bind_param("sssssi", $p_title, $p_when, $p_poster, $p_thumb, $p_facebook, $p_id);
+    return $statement->execute();
+}
+
+$success = $p_id == -1 ?  insertConcert() : updateConcert();
+if (!$success) {
+    http_response_code(500);
+    echo "Failed to update database";
+    return;
+}
---
+++ src/php/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$_db = null;
+
+function getOrCreateDb(): mysqli {
+    global $_db;
+    if ($_db == null) {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $_db = new mysqli('localhost', 'roathchamber', getenv('AMBER_ADMIN_PW'), 'amberDb');
+        if($_db->connect_errno) {
+            die("Failed to connect to MySQL: (" . $_db->connect_errno . ") " . $_db->connect_error);
+        }
+        $_db->set_charset('utf8');
+    }
+    return $_db;
+}
