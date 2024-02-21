9b6888ccebf57235986c81e3962d3b8ba0b8bd4c
David Kanekanian <dkanekanian1@gmail.com>
2024-02-21 11:51:24 +0000

    move to functions

--- src/php/concertEdit.php
+++ src/php/concertEdit.php
@@ -1,56 +1,85 @@
 <?php
 require_once __DIR__ . '/db.php';
 
-$p_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-$p_title = filter_input(INPUT_POST, 'title');
-$p_when = filter_input(INPUT_POST, 'when');
-$p_poster = filter_input(INPUT_POST, 'poster', FILTER_VALIDATE_URL);
-$p_thumb = filter_input(INPUT_POST, 'thumb', FILTER_VALIDATE_URL);
-$p_facebook = filter_input(INPUT_POST, 'facebook', FILTER_VALIDATE_URL);
-
-if (!$p_id || !$p_title || !strtotime($p_when) || !$p_poster || !$p_thumb || !$p_facebook) {
-    http_response_code(400);
-    echo "One or more invalid or missing parameter(s)";
-    return;
+function readParams() {
+    return [
+        'id' => filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT),
+        'title' => filter_input(INPUT_POST, 'title'),
+        'when' => filter_input(INPUT_POST, 'when'),
+        'poster' => filter_input(INPUT_POST, 'poster', FILTER_VALIDATE_URL),
+        'thumb' => filter_input(INPUT_POST, 'thumb', FILTER_VALIDATE_URL),
+        'facebook' => filter_input(INPUT_POST, 'facebook', FILTER_VALIDATE_URL),
+    ];
 }
 
-function insertConcert() {
+function validateParams($params) {
+    if(!$params['id'] || !$params['title'] || !strtotime($params['when']) ||
+       !$params['poster'] || !$params['thumb'] || !$params['facebook']
+    ) {
+        return [false, "One or more invalid or missing parameter(s)"];
+    }
+    return [true, ""];
+}
+
+function insertConcertStatement($params) {
     $statement = getOrCreateDb()->prepare(<<<SQL
         INSERT INTO `concerts`
         (`title`, `when`, `poster`, `thumb`, `facebook`)
         VALUES (?, ?, ?, ?, ?)
         SQL);
     if ($statement === false)
-        return [false, "Failed to prepare statement"];
-    if(!$statement->bind_param("sssss", $p_title, $p_when, $p_poster, $p_thumb, $p_facebook))
-        return [false, "Failed to bind parameters: $statement->error"];
-    try {
-        if (!$statement->execute())
-            return [false, "Failed to execute statement: " . $statement->error];
-    } catch (mysqli_sql_exception $e) {
-        return [false, "Failed to execute statement: " . $e->getMessage()];
-    }
-    return true;
+        return [null, "Failed to prepare statement"];
+    if(!$statement->bind_param("sssss", $params['title'], $params['when'], $params['poster'], $params['thumb'], $params['facebook']))
+        return [null, "Failed to bind parameters: $statement->error"];
+    return [$statement, ""];
 }
 
-function updateConcert() {
+function updateConcertStatement($params) {
     $statement = getOrCreateDb()->prepare(<<<SQL
         UPDATE `concerts`
         SET `title` = ?, `when` = ?, `poster` = ?, `thumb` = ?, `facebook` = ?
         WHERE `id` = ?
         SQL);
     if (!$statement)
-        return [false, "Failed to prepare statement"];
-    if(!$statement->bind_param("sssssi", $p_title, $p_when, $p_poster, $p_thumb, $p_facebook, $p_id))
-        return [false, "Failed to bind parameters: $statement->error"];
-    if(!$statement->execute())
-        return [false, "Failed to execute statement: $statement->error"];
-    return true;
+        return [null, "Failed to prepare statement"];
+    if(!$statement->bind_param("sssssi", $params['title'], $params['when'], $params['poster'], $params['thumb'], $params['facebook'], $params['id']))
+        return [null, "Failed to bind parameters: $statement->error"];
+    return [$statement, ""];
 }
 
-[$success, $errorMessage] = $p_id == -1 ?  insertConcert() : updateConcert();
-if (!$success) {
-    http_response_code(500);
-    echo "Failed to update database: $errorMessage";
-    return;
+function executeStatement($statement) {
+    try {
+        if (!$statement->execute()) {
+            return [false, "Failed to execute statement: $statement->error"];
+        }
+    } catch (mysqli_sql_exception $e) {
+        return [false, "Failed to execute statement (exception): " . $e->getMessage()];
+    }
+    return [true, ""];
 }
+
+function main() {
+    $params = readParams();
+    [$valid, $errorMessage] = validateParams($params);
+    if (!$valid) {
+        http_response_code(400);
+        echo "Validation Failed: $errorMessage";
+        return;
+    }
+
+    [$statement, $errorMessage] = $params['id'] == -1 ?  insertConcertStatement($params) : updateConcertStatement($params);
+    if ($statement === null) {
+        http_response_code(500);
+        echo "Failed to create statement: $errorMessage";
+        return;
+    }
+
+    [$success, $errorMessage] = executeStatement($statement);
+    if (!$success) {
+        http_response_code(500);
+        echo "Failed to execute statement: $errorMessage";
+        return;
+    }
+}
+
+main();
