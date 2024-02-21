3114941e47298d6038471d0613ac24fc116909aa
David Kanekanian <dkanekanian1@gmail.com>
2024-02-21 10:28:16 +0000

    time validate when

--- src/php/concertEdit.php
+++ src/php/concertEdit.php
@@ -2,13 +2,13 @@
 require_once __DIR__ . '/db.php';
 
 $p_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-$p_title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
-$p_when = filter_input(INPUT_POST, 'when', FILTER_SANITIZE_SPECIAL_CHARS);
+$p_title = filter_input(INPUT_POST, 'title');
+$p_when = filter_input(INPUT_POST, 'when');
 $p_poster = filter_input(INPUT_POST, 'poster', FILTER_VALIDATE_URL);
 $p_thumb = filter_input(INPUT_POST, 'thumb', FILTER_VALIDATE_URL);
 $p_facebook = filter_input(INPUT_POST, 'facebook', FILTER_VALIDATE_URL);
 
-if (!$p_id || !$p_title || !$p_when || !$p_poster || !$p_thumb || !$p_facebook) {
+if (!$p_id || !$p_title || !strtotime($p_when) || !$p_poster || !$p_thumb || !$p_facebook) {
     http_response_code(400);
     echo "One or more invalid or missing parameter(s)";
     return;
