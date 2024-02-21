131bb95e94a14f4187cdead4bf42258c95dfe7c1
David Kanekanian <dkanekanian1@gmail.com>
2024-02-21 10:42:17 +0000

    fix db user name

--- src/php/db.php
+++ src/php/db.php
@@ -6,7 +6,7 @@ function getOrCreateDb(): mysqli {
     global $_db;
     if ($_db == null) {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $_db = new mysqli('localhost', 'roathchamber', getenv('AMBER_ADMIN_PW'), 'amberDb');
+        $_db = new mysqli('localhost', 'amberAdmin', getenv('AMBER_ADMIN_PW'), 'amberDb');
         if($_db->connect_errno) {
             die("Failed to connect to MySQL: (" . $_db->connect_errno . ") " . $_db->connect_error);
         }
